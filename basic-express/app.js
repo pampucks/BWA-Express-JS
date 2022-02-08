8b63c42f688eb0fb25a97f69ccfaf7ec7bf4053f
pampucks <pampucks@gmail.com>
2022-02-08 12:02:27 +0700

    Initial Commit

---
+++ basic-express/app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.set('views', './views') //specify the views directory
+app.set('view engine', 'ejs') //register the template engine
+
+app.get('/', (req, res) => {
+    const buah = [
+        {name: 'apel'},
+        {name: 'melon'},
+        {name: 'mangga'}
+    ]
+    res.render('index', {
+        nama: 'fahmi ardiansyah',
+        umur: 21,
+        buah: buah
+    })
+})
+app.get('/:name', (req, res) => res.send(`Nama saya: ${req.params.name}`))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
